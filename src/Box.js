8546dee7f193427db132dad4aaca54933b8eae2e
William May <wjrm500@gmail.com>
2022-11-15 05:17:10 +0000

    Starting work on Grid

---
+++ src/Box.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Box = () => {
+  return (
+    <div className="outerBox">
+      <div className="innerBox">
+        Box
+      </div>
+    </div>
+  )
+}
+
+export default Box
--- src/Grid.js
+++ src/Grid.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import Box from './Box'
 
 const Grid = () => {
+  const dimension = 8
+  const rows = Array(dimension).fill().map(() => {
+    const row = Array(dimension).fill().map(() =>
+      <Box />
+    )
+    return <div className="row">{row}</div>
+  }
+  ) 
   return (
-    <div>Grid</div>
+    <div>
+      {rows}
+    </div>
   )
 }
 
