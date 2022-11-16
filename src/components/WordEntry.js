90939ae704b02a662c3b77c44772dca7e3c409ec
William May <wjrm500@gmail.com>
2022-11-16 02:04:29 +0000

    Controlling WordEntry input component with useState and onChange handler

--- src/components/WordEntry.js
+++ src/components/WordEntry.js
@@ -1,13 +1,26 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
+import { ACTION_CLICK_BOX, RequiredActionDispatchContext } from '../contexts/RequiredActionContext'
 
 const WordEntry = ({active}) => {
+  const requiredActionDispatch = useContext(RequiredActionDispatchContext)
+  const [value, setValue] = useState("")
+  const onChange = (e) => {
+    setValue(e.target.value)
+  }
+  const onKeyDown = (e) => {
+    if (e.key == "Enter") {
+      setValue("")
+      requiredActionDispatch(ACTION_CLICK_BOX)
+    }
+  }
+  
   return (
     <div id="wordEntryContainer" className={active ? "active" : "inactive"}>
         <div className="labelContainer">
             <label>Enter a word</label>
         </div>
         <div className="inputContainer">
-            <input id="word-entry" type="text" disabled />
+            <input ref={input => input && input.focus()} id="wordEntry" type="text" value={value} disabled={!active} onChange={onChange} onKeyDown={onKeyDown}  />
         </div>
     </div>
   )
