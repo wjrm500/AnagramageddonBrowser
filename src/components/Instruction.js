0d1183e23f196eaabfb8673badd310cdf255f7a1
William May <wjrm500@gmail.com>
2022-11-16 03:05:41 +0000

    Changing colour of active player's name in instruction box at top

--- src/components/Instruction.js
+++ src/components/Instruction.js
@@ -7,7 +7,7 @@ const Instruction = () => {
   const secondsRemaining = 5
   return (
     <div id="instruction">
-      It's <span id="activePlayer">{activePlayer.name}</span>'s turn! {prompt}. You've got <span id="timer">{secondsRemaining}</span> seconds...
+      It's <span id="activePlayer" style={{color: activePlayer.color}}>{activePlayer.name}</span>'s turn! {prompt}. You've got <span id="timer">{secondsRemaining}</span> seconds...
     </div>
   )
 }
